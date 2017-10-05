7c2b12563c6270583f37de48746d5cca668f2914
PensCup87 <bdt9123@gmail.com>
2017-10-05 15:13:42 -0400

    OOP FrameWork

--- week2 _day4_IntroTo_OOP/Cat.cs
+++ week2 _day4_IntroTo_OOP/Cat.cs
@@ -8,5 +8,32 @@ namespace week2__day4_IntroTo_OOP
 {
     class Cat
     {
+        //STATES
+        //fields
+        private string name;
+
+        //properties
+        //a characteristic that is an action word
+        //this allows/denies code to interact with fields
+        public string Name
+        {
+            //typical format
+            get { return this.name; }
+            set { this.name = value; }
+        }
+
+        
+        
+        //BEHAVIORS
+
+        
+        
+        //CONSTRUCTORS
+        public Cat()
+            //A Constructor with blank () - is a default Constrcutor because it takes no
+            //parameters; when the () are empty and the body can also be empty
+        {
+
+        }
     }
 }
